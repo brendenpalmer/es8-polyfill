5bf42251e9d1194a1f4a75f6088085ff7dbf82c3
Brenden Palmer <brendenpalmer@gmail.com>
2017-09-03 15:19:58 -0400

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,7 @@ fi
 git add lib/\*
 git add docs/\*
 git commit --message "Travis build $TRAVIS_BUILD_NUMBER"
-git push origin ${$TRAVIS_BRANCH}
+#git remote add origin-es8-polyfill https://${GH_TOKEN}@github.com/brendenpalmer/es8-polyfill.git > /dev/null 2>&1
+git push origin ${TRAVIS_BRANCH}
 
 echo "TRAVIS BRANCH: $TRAVIS_BRANCH"

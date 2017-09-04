7ec1018ae7ef3596c99848f133a970da34d6142a
Brenden Palmer <brendenpalmer@gmail.com>
2017-09-04 01:12:34 -0400

    Update auto-generated files, travis CI build:  [skip ci]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,9 +22,9 @@ git checkout ${TRAVIS_BRANCH}
 
 if [ $(git status --untracked-files=no --porcelain) ]
 then
-  echo "There are no changes so there is no need to update repo"
-  exit 0;
-else
   git commit --no-verify -a --message "Update auto-generated files, travis CI build: $TRAVIS_BUILD_NUMBER [skip ci]"
   git push --quiet --set-upstream origin-es8-polyfill ${TRAVIS_BRANCH}
+else
+  echo "There are no changes so there is no need to update repo"
+  exit 0;
 fi

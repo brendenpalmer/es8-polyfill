23857fe4f3e0072a55e9361f5cfdb273fbaa41d8
Brenden Palmer <brendenpalmer@gmail.com>
2017-09-04 01:10:04 -0400

    Update auto-generated files, travis CI build:  [skip ci]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
+
+set -o errexit -o nounset
+
+if [ ${TRAVIS_PULL_REQUEST} != "false" ]
+then
+  echo "Skipping deployment because this is a PR build"
+  exit 0
+fi
+
+if [ ${TRAVIS_BRANCH} != "master" ]
+then
+  echo "This commit was made against $TRAVIS_BRANCH. Only builds on master will be deployed."
+  exit 0
+fi
+
+git config user.email "travis@travis-ci.org"
+git config user.name "Travis CI"
+git remote add origin-es8-polyfill https://${GH_TOKEN}@github.com/brendenpalmer/es8-polyfill.git > /dev/null 2>&1
+git fetch
+git checkout ${TRAVIS_BRANCH}
+
 if [ $(git status --untracked-files=no --porcelain) ]
 then
   echo "There are no changes so there is no need to update repo"

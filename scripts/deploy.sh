bbbff85bbf98ad28c45f5b100b1963c75ad0a285
Brenden Palmer <brendenpalmer@gmail.com>
2017-09-04 01:13:46 -0400

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,11 +20,11 @@ git remote add origin-es8-polyfill https://${GH_TOKEN}@github.com/brendenpalmer/
 git fetch
 git checkout ${TRAVIS_BRANCH}
 
-if [ $(git status --untracked-files=no --porcelain) ]
+if [[ $(git status --untracked-files=no --porcelain) ]]
 then
-  echo "There are no changes so there is no need to update repo"
-  exit 0;
-else
   git commit --no-verify -a --message "Update auto-generated files, travis CI build: $TRAVIS_BUILD_NUMBER [skip ci]"
   git push --quiet --set-upstream origin-es8-polyfill ${TRAVIS_BRANCH}
+else
+  echo "There are no changes so there is no need to update repo"
+  exit 0;
 fi
